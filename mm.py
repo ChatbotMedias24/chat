f1684beebb874e59f9f50f33072cb096d0e0f7d1
Chatbot <134295092+ChatbotMedias24@users.noreply.github.com>
2023-05-29 09:00:57 +0100

    Update mm.py

--- mm.py
+++ mm.py
@@ -53,7 +53,17 @@ with textcontainer:
 # Afficher l'image dans la barre latérale
     st.sidebar.image(logo_path, width=200) 
     st.sidebar.subheader("Suggestions:")
-    selected_question = st.sidebar.selectbox("Choisir :", ["","Pouvez vous me donner le résumé du rapport ?"," Quels sont les principaux défis auxquels le Maroc est confronté en matière de droits socio-économiques ?", "quel est le nombre de plainte reçues en 2022", "Sur quels éléments repose l'approche reflexive thinking proposée par le Conseil ?","En quoi consiste l'approche reflexive thinking proposée par le Conseil ?"])
+    questions = [
+    "Pouvez vous me donner le résumé du rapport ?",
+    "Quels sont les principaux défis auxquels le Maroc est confronté en matière de droits socio-économiques ?",
+    "Quel est le nombre de plaintes reçues en 2022 ?",
+    "Sur quels éléments repose l'approche reflexive thinking proposée par le Conseil ?",
+    "En quoi consiste l'approche reflexive thinking proposée par le Conseil ?"
+]
+    selected_questions = []
+    for question in questions:
+     if st.sidebar.checkbox(question):
+        selected_questions.append(question) 
     if selected_question:
         question = selected_question
         if question:
