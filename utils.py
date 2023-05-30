a5deb86337df56540f1dac0c385c2663a0e778eb
Chatbot <134295092+ChatbotMedias24@users.noreply.github.com>
2023-05-30 14:45:48 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -6,10 +6,12 @@ openai.api_key = st.secrets["open_api_key"]
 model = SentenceTransformer('all-MiniLM-L6-v2')
 
 pinecone.init(api_key=st.secrets["pincone_key"], environment=st.secrets["environement"])
-index = pinecone.Index('testchat')
+index = pinecone.Index('testi')
 
 def find_match(input):
     input_em = model.encode(input).tolist()
     result = index.query(input_em, top_k=2, includeMetadata=True)
     return result['matches'][0]['metadata']['text']+"\n"+result['matches'][1]['metadata']['text']
+
+
 
